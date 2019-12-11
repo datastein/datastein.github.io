7b56a0a975974c60b3fca533fb2b79e2417c8909
datastein <mj_stein@hotmail.com>
2019-12-10 19:18:51 -0800

    %1

--- pelicanconf.py
+++ pelicanconf.py
@@ -54,7 +54,8 @@ SITENAME = 'Jeanette\'s Data Blog'
 SITETITLE = 'Jeanette\'s Data Blog'
 SITESUBTITLE = 'Blogging in Seattle'
 SITEDESCRIPTION = 'Jeanette\'s Thoughts and Writings'
-SITELOGO = SITEURL + 'pelican/img/JS_profile.png'
+SITELOGO = "JS_profile.png"
+#SITELOGO = SITEURL + 'pelican/img/JS_profile.png'
 #SITELOGO = SITEURL + '/images/JS_profile.png'
 BROWSER_COLOR = '#1569C7'
 
